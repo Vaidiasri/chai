b78373d8b1ac27d19a1969ab953de25a10e961b8
vaibhavghildiyal <vaibhavghildiyal2101@gmail.com>
2026-04-28 23:39:29 +0530

    api

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 //require("dotenv").config({path:"./env"})
 import connection from "./db/db.js";
+import  app from "./app.js"
+
 import dotenv from "dotenv"
 dotenv.config({
     path:"./env"
 })
 
 connection()
+.then(()=>{
+    app.listen(process.env.Port||8000,()=>{
+        console.log(`Server is runing at:${process.env.Port}`)
+    })
+})
+.catch((err)=>{
+  console.log(err)
+})
