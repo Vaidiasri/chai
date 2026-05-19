910858e8dd3ea5d225fd8925c877f679ba145938
vaibhavghildiyal <vaibhavghildiyal2101@gmail.com>
2026-05-20 00:00:50 +0530

    update

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,9 +1,25 @@
 import asyncHandler from "../utils/asyncHandler.js";
+import ApiError from "../utils/ApiError.js";
+import { User } from "../models/user.model.js";
+
 const registerUser=asyncHandler(async (req, res)=>{ // basic controller method
     // res.status(200).json({
     //     message:"hello  😁"
     // })
     const  {userName,email,fullName,avatar,coverImage,password}=req.body
     console.log(`email : ${email}`)
+    // validation 
+    if(
+        [fullName,email,userName,avatar,coverImage,password].some((filed)=>filed?.trim()=="")
+        
+    ){
+        {
+            throw new ApiError(400,"You miss some  required detail")
+        }
+    }
+    // user already exist 
+    const exist=User.find({
+        $or:[{username},{email}]
+    })
 })
 export default registerUser;
